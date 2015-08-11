b7fa30049c1106bcdbe199ab0fd85ddb7991f670
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-08-11 17:50:44 +0530

    Functor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,9 @@
 object Main {
-       def main(args: Array[String]): Unit = {
-       	   println("Main Application")
-       }
+  def main(args: Array[String]): Unit = {
+    println("Main Application")
+  }
+}
+
+trait Functor[F[_]] {
+  def map[A, B](a: F[A])(f: A => B): F[B]
 }
